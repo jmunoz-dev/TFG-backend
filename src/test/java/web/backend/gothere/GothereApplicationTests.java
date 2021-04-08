ae4b17dfce2e5c3ca02ac2e67cf9d6d326c8b395
Rubén Valero <rubenvillarreal2012@gmail.com>
2021-04-08 17:48:12 +0200

    first commit

---
+++ src/test/java/web/backend/gothere/GothereApplicationTests.java
@@ -0,0 +1,13 @@
+package web.backend.gothere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GothereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
