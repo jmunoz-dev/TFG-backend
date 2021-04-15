7ef9d8fbe6009ed5495c3e4d8d89847859910c5b
Rubén Valero <rubenvillarreal2012@gmail.com>
2021-04-15 19:56:41 +0200

    delete some imports

--- src/main/java/web/backend/gothere/repositories/entities/ConfirmationTokenEntity.java
+++ src/main/java/web/backend/gothere/repositories/entities/ConfirmationTokenEntity.java
@@ -10,9 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import web.backend.gothere.Repositories.Entities.UserEntity;
-
 @Table(name="ConfirmationTokens")
 @Entity(name="ConfirmationTokens")
 public class ConfirmationTokenEntity {
