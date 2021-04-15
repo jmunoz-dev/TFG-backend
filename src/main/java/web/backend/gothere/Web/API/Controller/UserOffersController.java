81e5db304aa895e8f8221dcc36b10b4e2ac0b7a1
Rubén Valero <rubenvillarreal2012@gmail.com>
2021-04-15 19:58:51 +0200

    added delete CRUD useroffer controller

--- src/main/java/web/backend/gothere/Web/API/Controller/UserOffersController.java
+++ src/main/java/web/backend/gothere/Web/API/Controller/UserOffersController.java
@@ -3,10 +3,12 @@ package web.backend.gothere.Web.API.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import web.backend.gothere.Services.UserOffersService;
 import web.backend.gothere.Services.Models.UserOfferDTO;
@@ -22,13 +24,20 @@ public class UserOffersController {
     }
 
     @PostMapping()
-    public UserOfferDTO AddUserOffer(@RequestBody UserOfferDTO userOffer){
+    public String AddUserOffer(@RequestBody UserOfferDTO userOffer){
         return userOffersService.add(userOffer);
     }
     @GetMapping()
     public List<UserOfferDTO> GetAll(){
         return userOffersService.getAll();
     }
-    
+    @DeleteMapping
+    public void deleteUserOffer(@RequestParam String code){
+        userOffersService.deleteUserOffer(code);
+    }
+    // @GetMapping()
+    // public List<UserOfferDTO> getByUserAndBar(@RequestParam Long idBar,@RequestParam Long idUser){
+    //     return userOffersService.
+    // }
 
 }
