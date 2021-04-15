b0605e0138326299960d51eee57229f7fba32d92
Rubén Valero <rubenvillarreal2012@gmail.com>
2021-04-15 19:57:23 +0200

    delete userOffer and code generator control

--- src/main/java/web/backend/gothere/Services/UserOffersService.java
+++ src/main/java/web/backend/gothere/Services/UserOffersService.java
@@ -4,22 +4,20 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
-
-import org.apache.catalina.startup.Tomcat.ExistingStandardWrapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-import web.backend.gothere.UserAlreadyExistException;
+import web.backend.gothere.Exceptions.ElementNotFoundException;
+import web.backend.gothere.Exceptions.OfferCodeException;
+import web.backend.gothere.Exceptions.UserAlreadyExistException;
 import web.backend.gothere.Repositories.Entities.OfferEntity;
 import web.backend.gothere.Repositories.Entities.UserEntity;
 import web.backend.gothere.Repositories.Entities.UserOfferEntity;
 import web.backend.gothere.Repositories.Interfaces.OffersRepository;
 import web.backend.gothere.Repositories.Interfaces.UserOfferRepository;
 import web.backend.gothere.Repositories.Interfaces.UserRepository;
-import web.backend.gothere.Services.Models.OfferDTO;
-import web.backend.gothere.Services.Models.UserDTO;
 import web.backend.gothere.Services.Models.UserOfferDTO;
 
 public class UserOffersService {
@@ -34,59 +32,74 @@ public class UserOffersService {
     @Autowired
     private ModelMapper modelMapper;
 
-    public UserOfferDTO add(UserOfferDTO userOffer){
-       Optional<UserOfferEntity> userOfferTemp= UserOfferRepository.findByUserAndOffer(
-          UserRepository.findByEmail(userOffer.getUser().getEmail()).get(), OfferRepository.findById(userOffer.getOffer().getIdOffer()).get());
+    public String add(UserOfferDTO userOffer){
+        Optional<UserOfferEntity> userOfferTemp = UserOfferRepository.findByUserAndOffer(
+                UserRepository.findByEmail(userOffer.getUser().getEmail()).get(),
+                OfferRepository.findById(userOffer.getOffer().getIdOffer()).get());
 
-        if(userOfferTemp.isPresent()){
+        if (userOfferTemp.isPresent()) {
             throw new UserAlreadyExistException();
         }
         boolean isUnique = false;
-        do{
+        int count = 0;
+        do {
             String code = generateCode();
-            Optional<UserOfferEntity> exist = UserOfferRepository.findByCode(code);
-            if(!exist.isPresent()){
-                 isUnique = true;
-                 userOffer.setCode(code);
+            Optional<UserOfferEntity> exist = UserOfferRepository.findByCode(code.toUpperCase());
+            if (!exist.isPresent()) {
+                isUnique = true;
+                userOffer.setCode(code);
             }
-        }while(!isUnique);
+            count++;
+            if(count > 1000){
+                throw new OfferCodeException();
+            }
+        } while (!isUnique);
         Optional<UserEntity> user = UserRepository.findByEmail(userOffer.getUser().getEmail());
         Optional<OfferEntity> offer = OfferRepository.findById(userOffer.getOffer().getIdOffer());
         UserOfferEntity entityToInsert = modelMapper.map(userOffer, UserOfferEntity.class);
-        if(user.isPresent() && offer.isPresent()){
+        if (user.isPresent() && offer.isPresent()) {
             entityToInsert.setOffer(offer.get());
             entityToInsert.setUser(user.get());
-        }else{
-            throw new UserAlreadyExistException();
+        } else {
+            throw new ElementNotFoundException();
         }
-        
-        UserOfferEntity result = UserOfferRepository.save(entityToInsert);
-       return modelMapper.map(result, UserOfferDTO.class);
-    }
-    public List<UserOfferDTO> getAll() throws ResponseStatusException {
-        
-        List<UserOfferDTO> userOffersList = UserOfferRepository.findAll().stream()
-        .map(x -> modelMapper.map(x, UserOfferDTO.class))
-        .collect(Collectors.toList());
 
-        if(!userOffersList.isEmpty()){
+        UserOfferEntity result = UserOfferRepository.save(entityToInsert);
+        return result.getCode();
+    }
+
+    public List<UserOfferDTO> getAll() throws ResponseStatusException {
+
+        List<UserOfferDTO> userOffersList = UserOfferRepository.findAll().stream()
+                .map(x -> modelMapper.map(x, UserOfferDTO.class)).collect(Collectors.toList());
+
+        if (!userOffersList.isEmpty()) {
             return userOffersList;
-        }else{
+        } else {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error finding data");
         }
     }
-    private String generateCode(){
+
+    private String generateCode() {
         int leftLimit = 48; // numeral '0'
         int rightLimit = 122; // letter 'z'
         int targetStringLength = 10;
         Random random = new Random();
 
         String generatedString = random.ints(leftLimit, rightLimit + 1)
-        .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-        .limit(targetStringLength)
-        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-        .toString();
-    
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+
         return generatedString.toUpperCase();
     }
+    public void deleteUserOffer(String code){
+        Optional<UserOfferEntity> entityToDelete = UserOfferRepository.findByCode(code.toUpperCase());
+        if(entityToDelete.isPresent()){
+            UserOfferRepository.delete(entityToDelete.get());
+        }
+    }
+    // public List<UserOfferDTO> getByUserAndBar(Long idBar, Long idUser){
+    //     Optional<UserEntity> user = UserRepository.findById(idUser);
+    // }
+
 }
