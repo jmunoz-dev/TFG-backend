d69b0c1e282e38ea59bc1b3c4ee27363e43ca7ea
jmunoz-dev <jorge98mv@gmail.com>
2021-04-19 17:09:57 +0200

    InitDB on feature/ofertas

--- src/main/java/web/backend/gothere/Web/Config/InitDB.java
+++ src/main/java/web/backend/gothere/Web/Config/InitDB.java
@@ -8,15 +8,17 @@ import org.springframework.context.annotation.Configuration;
 
 import web.backend.gothere.Repositories.Entities.BarEntity;
 import web.backend.gothere.Repositories.Entities.OfferEntity;
+import web.backend.gothere.Repositories.Interfaces.BarRepository;
 import web.backend.gothere.Repositories.Interfaces.OffersRepository;
 
 @Configuration
 public class InitDB {
     @Bean
-    CommandLineRunner initDatabase(OffersRepository offersRepository) {
+    CommandLineRunner initDatabase(OffersRepository offersRepository, BarRepository barRepository) {
         return args -> {
+            barRepository.save(new BarEntity(45L, 5569L, 5569L, "name", "description", "direction", "66666666", "schedule", 25, 90, 80));
             for(int i = 1; i<=10; i++){   
-                offersRepository.save(new OfferEntity("titulo oferta", "desciption", "img.png", 22.50, 15, 0, LocalDate.now(), LocalDate.of(2022, 01, 01), new BarEntity(45L, 5569L, 5569L, "name", "description", "direction", "66666666", "schedule", 25, 90, 80)));
+                offersRepository.save(new OfferEntity("titulo oferta "+i, "desciption", "img.png", 22.50, 15, 0, LocalDate.now(), LocalDate.of(2022, 01, 01), barRepository.findById(1L).get() ));
             }            
         };
     }
