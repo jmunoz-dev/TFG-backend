8ac92d2c3612bee9924b034eda796e3b39765b88
ruben227 <rubenvillarreal2012@gmail.com>
2021-04-21 23:17:46 +0200

    find by user and bar

--- src/main/java/web/backend/gothere/Repositories/Interfaces/UserOfferRepository.java
+++ src/main/java/web/backend/gothere/Repositories/Interfaces/UserOfferRepository.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+
+import web.backend.gothere.Repositories.Entities.BarEntity;
 import web.backend.gothere.Repositories.Entities.OfferEntity;
 import web.backend.gothere.Repositories.Entities.UserEntity;
 import web.backend.gothere.Repositories.Entities.UserOfferEntity;
@@ -15,10 +17,10 @@ public interface UserOfferRepository extends JpaRepository<UserOfferEntity, Long
     @Query(value = "SELECT uo FROM UserOffers uo WHERE uo.User = :idUser AND uo.offer = :idOffer")
     Optional<UserOfferEntity> findByUserAndOffer(@Param("idUser") UserEntity idUser,@Param("idOffer") OfferEntity idOffer);
 
-    // @Query(value = "SELECT uo FROM UserOffers uo "+
-    // "INNER JOIN Offers o "+
-    // "ON o.idOffer = ou.idOffer "+
-    //  "WHERE uo.User = :user AND o.Bar = :b")
-    // List<UserOfferEntity> findByUserAndBar(@Param("user") UserEntity user,  @Param("bar") OfferEntity bar);
+    @Query(value = "SELECT uo FROM UserOffers uo "+
+    "FULL JOIN Offers o "+
+    "ON o.idOffer = ou.idOffer "+
+     "WHERE uo.User = :user AND o.Bar = :bar")
+    List<UserOfferEntity> findByUserAndOfferBar(@Param("user") UserEntity user,@Param("bar") BarEntity bar);
 
 }
