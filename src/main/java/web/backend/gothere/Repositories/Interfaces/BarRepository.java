0f2ca9447341dbc146b7a1891ffa5272fd2d11ae
ruben227 <rubenvillarreal2012@gmail.com>
2021-04-27 17:27:09 +0200

    fixed search query

--- src/main/java/web/backend/gothere/Repositories/Interfaces/BarRepository.java
+++ src/main/java/web/backend/gothere/Repositories/Interfaces/BarRepository.java
@@ -1,7 +1,7 @@
 package web.backend.gothere.Repositories.Interfaces;
 
 import java.util.Collection;
-
+import web.backend.gothere.Constants;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,8 +11,11 @@ import web.backend.gothere.Repositories.Entities.BarEntity;
 public interface BarRepository 
     extends JpaRepository<BarEntity,Long>{
    
-        @Query(value = "SELECT b FROM Bars b WHERE  ((b.length < :length + 5) AND (b.length > :length - 5)) AND ((b.latitude< :latitude + 5) AND (b.latitude > :latitude - 5))")
-        Collection<BarEntity> getByCoordinates(@Param("latitude")double latitude, @Param("length")double length);
+        @Query(value = "SELECT b FROM Bars b WHERE" +
+        "((b.length < :length + " +Constants.BAR_SEARCH_RADIUS +" ) AND (b.length > :length - " + Constants.BAR_SEARCH_RADIUS+"))"
+        +" AND " +
+        " ((b.latitude< :latitude + " +  Constants.BAR_SEARCH_RADIUS+") AND (b.latitude > :latitude - " + Constants.BAR_SEARCH_RADIUS + "))")
+        Collection<BarEntity> getByCoordinates(@Param("latitude") double latitude, @Param("length") double length);
 
    
 }
