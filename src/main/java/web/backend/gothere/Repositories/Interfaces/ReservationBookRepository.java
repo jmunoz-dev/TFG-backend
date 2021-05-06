e542082b7f14dec035f52691029e7c328e1bcccf
ruben227 <rubenvillarreal2012@gmail.com>
2021-05-06 18:40:40 +0200

    find by reservations by user

--- src/main/java/web/backend/gothere/Repositories/Interfaces/ReservationBookRepository.java
+++ src/main/java/web/backend/gothere/Repositories/Interfaces/ReservationBookRepository.java
@@ -1,12 +1,17 @@
 package web.backend.gothere.Repositories.Interfaces;
 
 
+import java.util.Collection;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import web.backend.gothere.Repositories.Entities.ReservationBookEntity;
+import web.backend.gothere.Repositories.Entities.UserEntity;
 
 public interface ReservationBookRepository 
     extends JpaRepository<ReservationBookEntity, Long> {
     
+    Collection<ReservationBookEntity> findByUser(UserEntity user);
     Optional<ReservationBookEntity> findById(Long id);
 }
--- src/main/java/web/backend/gothere/Services/ReservationBookService.java
+++ src/main/java/web/backend/gothere/Services/ReservationBookService.java
@@ -8,12 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import web.backend.gothere.Exceptions.ElementNotFoundException;
+import web.backend.gothere.Repositories.Entities.ReservationBookEntity;
+import web.backend.gothere.Repositories.Entities.UserEntity;
 import web.backend.gothere.Repositories.Interfaces.ReservationBookRepository;
+import web.backend.gothere.Repositories.Interfaces.UserRepository;
 import web.backend.gothere.Services.Models.ReservationBookDTO;
+import web.backend.gothere.Services.Models.UserDTO;
 
 public class ReservationBookService {
     @Autowired
     private ReservationBookRepository reservationBookRepository;
+
+    @Autowired
+    private UserRepository userRepository;
     @Autowired
     private ModelMapper modelMapper;
 
@@ -30,6 +38,32 @@ public class ReservationBookService {
         }
     }
 
+    // find user reservations 
+    public List<ReservationBookDTO> getByUser(Long idUser, boolean currentReservation) throws ResponseStatusException {
+        Optional<UserEntity> user = userRepository.findById(idUser);
+        if(!user.isPresent()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not foud");
+        }
+
+        List<ReservationBookDTO> reservationList = reservationBookRepository.findByUser(user.get()).stream()
+        .map(x -> modelMapper.map(x, ReservationBookDTO.class))
+        .collect(Collectors.toList());
+        
+        if(reservationList.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error finding data");
+        }
+        if(!currentReservation){
+            return reservationList;
+        }
+        List<ReservationBookDTO> currentReservations = new ArrayList<>();
+        for(int i = 0 ; i < reservationList.size(); i++){
+            if(reservationList.get(i).getCheckOutHour().isBefore(LocalDateTime.now())){
+                currentReservations.add(reservationList.get(i));
+            }
+        }
+        return currentReservations;
+
+    }
     public ReservationBookDTO getById(Long reBoId){
         Optional<ReservationBookEntity> resevation =  reservationBookRepository.findById(reBoId);
         if(resevation.isPresent()){
