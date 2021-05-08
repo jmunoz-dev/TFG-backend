4022aa5f2b0b1d7d11d7fb2af9d25de33936f300
ruben227 <rubenvillarreal2012@gmail.com>
2021-05-08 15:30:21 +0200

    added find by date and table on reservation entity

--- src/main/java/web/backend/gothere/Repositories/Interfaces/ReservationBookRepository.java
+++ src/main/java/web/backend/gothere/Repositories/Interfaces/ReservationBookRepository.java
@@ -1,12 +1,15 @@
 package web.backend.gothere.Repositories.Interfaces;
 
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import web.backend.gothere.Repositories.Entities.BarTableEntity;
 import web.backend.gothere.Repositories.Entities.ReservationBookEntity;
+import web.backend.gothere.Repositories.Entities.ScheduleTableReservationEntity;
 import web.backend.gothere.Repositories.Entities.UserEntity;
 
 public interface ReservationBookRepository 
@@ -14,4 +17,5 @@ public interface ReservationBookRepository
     
     Collection<ReservationBookEntity> findByUser(UserEntity user);
     Optional<ReservationBookEntity> findById(Long id);
+    Collection<ReservationBookEntity> findByReservationDateAndScheduleTableReservationBarTable(LocalDate date,BarTableEntity barTable);
 }
