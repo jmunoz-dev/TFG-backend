0acbadc1b86183cde76cc5bfc7122f069c541b18
ruben227 <rubenvillarreal2012@gmail.com>
2021-05-08 19:05:41 +0200

    created basic reservation controller

---
+++ src/main/java/web/backend/gothere/Web/API/Controller/ReservationBookController.java
@@ -0,0 +1,28 @@
+package web.backend.gothere.Web.API.Controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import web.backend.gothere.Services.ReservationBookService;
+import web.backend.gothere.Services.Models.ReservationBookDTO;
+
+@RestController
+@RequestMapping("api/reservations")
+public class ReservationBookController {
+    private final ReservationBookService reservationBookService;
+
+    ReservationBookController (ReservationBookService reservationBookService) {
+        this.reservationBookService = reservationBookService;
+    }
+
+
+    @GetMapping("/{idUser}")
+    public List<ReservationBookDTO> getByUserAndBar(@PathVariable("idUser") Long idUser, @RequestParam(required = false , value = "current") boolean current){
+        return reservationBookService.getByUser(idUser, current);
+    }
+}
