3bda42f731461ef5ed31d4d17b1fde12e7fa7f94
ruben227 <rubenvillarreal2012@gmail.com>
2021-05-18 20:39:05 +0200

    added admin login js

---
+++ src/main/resources/static/js/admin-login.js
@@ -0,0 +1,28 @@
+
+window.onload = () =>{
+    document.forms[0].onsubmit = (e) => {
+        e.preventDefault();
+        let user = {
+            email : document.forms[0].email.value,
+            password : document.forms[0].password.value
+        }
+    
+        fetch('/api/users/sign-in-bar', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data, user);
+            if(data === true)
+                window.location = "/admin/home"
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    } 
+}
+
