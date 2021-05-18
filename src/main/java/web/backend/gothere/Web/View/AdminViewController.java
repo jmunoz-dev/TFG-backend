c4a295c1203dac28bf8f3c33d8349929deb1a080
ruben227 <rubenvillarreal2012@gmail.com>
2021-05-18 20:38:17 +0200

    check if cookie exist on home controller

--- src/main/java/web/backend/gothere/Web/View/AdminViewController.java
+++ src/main/java/web/backend/gothere/Web/View/AdminViewController.java
@@ -1,12 +1,14 @@
 package web.backend.gothere.Web.View;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import web.backend.gothere.Services.BarService;
 import web.backend.gothere.Services.UserService;
+import web.backend.gothere.Services.Models.UserDTO;
 
 @Controller
 @RequestMapping("admin")
@@ -23,15 +25,26 @@ public class AdminViewController {
 
     @GetMapping("")
     public ModelAndView barLoginPage(){
+        //TODO mirar que esté la cookie
         ModelAndView mv = new ModelAndView("login");
+        UserDTO user = new UserDTO();
+        mv.addObject("user", user);
         mv.addObject("bar", barService.getBarById(3L));
         return mv;
     } 
 
     @GetMapping("/home")
-    public ModelAndView barHomePage(){
+    public ModelAndView barHomePage(@CookieValue( required = false, value="adminlogin") String cookie ){
+        
+        if(cookie == null){
+            ModelAndView mv2 = new ModelAndView("redirect:/admin");
+            return mv2;
+        }
         ModelAndView mv = new ModelAndView("admin_home");
+        //TODO usuario por token
+        //bar por usuario
         mv.addObject("bar", barService.getBarById(3L));
+
         return mv;
     } 
 
