938e10c23c05f6a5a7d231f1fbb64c15d2e0251f
Jorge Muñoz <jorge98mv@gmail.com>
2021-05-23 01:10:25 +0200

    Fields and fetch to PUT controller

--- src/main/resources/static/js/admin-sections.js
+++ src/main/resources/static/js/admin-sections.js
@@ -1,29 +1,39 @@
 window.onload = () => {
+
+    const BASE_URL = 'http://localhost:8080/'
+
     if (/admin\/home/.test(window.location.href)) {
         let button = document.querySelector("button.changes-button")
         button.addEventListener('click', function(event) {
             let bar = {
-                idbar: document.querySelector('#idbar'),
-                description: document.querySelector('#description'),
-                address: document.querySelector('#address'),
+                idBar: document.querySelector('#idbar').value,
+                name: document.querySelector('#name').innerHTML,
+                description: document.querySelector('#description').value,
+                address: document.querySelector('#address').value,
+                phone: document.querySelector('#phone').value,
+                schedule: document.querySelector('#schedule').value,
+                currentCapacity: document.querySelector('#currentCapacity').value,
+                totalCapacity: document.querySelector('#totalCapacity').value,
+                allowedCapacity: document.querySelector('#allowedCapacity').value,
+                length: document.querySelector('#length').value,
+                latitude: document.querySelector('#latitude').value,
             }
-            alert(bar)
-                // fetch('/api/bars/', {
-                //         method: 'PUT',
-                //         headers: {
-                //             'Content-Type': 'application/json',
-                //         },
-                //         body: JSON.stringify(bar),
-                //     })
-                //     .then(response => response.json())
-                //     .then(data => {
-                //         console.log('Success:', data, bar);
-                //         if (data === true)
-                //             window.location = "/admin/home"
-                //     })
-                //     .catch((error) => {
-                //         console.error('Error:', error);
-                //     });
+            fetch(BASE_URL + '/api/bars/' + idbar, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(bar),
+                })
+                .then(response => console.log(response))
+                .then(data => {
+                    console.log('Success:', data, bar);
+                    if (data === true)
+                        window.location = "/admin/home"
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
         })
     }
 }
