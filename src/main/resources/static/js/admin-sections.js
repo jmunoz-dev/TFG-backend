b9db883e210d015d17a87878ee20630f2f8f5d20
Jorge Muñoz <jorge98mv@gmail.com>
2021-05-23 20:35:15 +0200

    Popup to activate offer

--- src/main/resources/static/js/admin-sections.js
+++ src/main/resources/static/js/admin-sections.js
@@ -34,4 +34,71 @@ window.onload = () => {
                 });
         })
     }
+
+    if (/admin\/offers/.test(window.location.href)) {
+
+        let modal_popup =
+            `<div class="modal hidden" id="modal-one">
+            <div class="modal-container">
+                <h1>Activar código</h1>
+                <div class="modal-body">
+                    <input id="code-activation" class="code-activation" name="code-activation" />
+                    <button id="modal-submit" class="modal-submit">Comprobar y activar</button>
+                </div>        
+            <button id="close-modal" class="modal-close modal-exit">X</button>
+            </div>
+            </div>`;
+        document.querySelector('body').insertAdjacentHTML('beforeend', modal_popup)
+
+        let buttonShowModal = document.querySelector("button#activation")
+        let buttonActivate = document.querySelector("button#modal-submit")
+        let buttonCloseModal = document.querySelector("button#close-modal")
+
+        let buttonCreateNew = document.querySelector("button#creation")
+
+
+        buttonShowModal.addEventListener('click', function(event) {
+            document.querySelector('#modal-one').classList.toggle('hidden')
+            document.querySelector('#modal-one').classList.toggle('shown')
+        })
+
+        buttonActivate.addEventListener('click', function(event) {
+            let codeValue = document.querySelector("input#code-activation").value
+            let offer = {
+                //TODO: construir objeto oferta
+            }
+            fetch('/api/userOffers/' + codeValue, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status === 404) {
+                        alert('No se ha podido activar. Aquí mejor modificar modal y que salga algun ok o algo asi')
+                    }
+                    if (data.status === 200) {
+                        alert('Activada correctamente')
+                    }
+                    window.location = "/admin/offers"
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                    alert('No aceptada')
+                });
+
+
+        })
+
+        buttonCloseModal.addEventListener('click', function(e) {
+            document.querySelector('#modal-one').classList.toggle('hidden')
+            document.querySelector('#modal-one').classList.toggle('shown')
+        })
+
+
+
+
+
+    }
 }
