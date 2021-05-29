ccb30d5f6693929dda1890670d0e268673986a50
Jorge Muñoz <jorge98mv@gmail.com>
2021-05-29 14:05:08 +0200

    fix names

--- src/main/java/web/backend/gothere/Web/API/Controller/OffersController.java
+++ src/main/java/web/backend/gothere/Web/API/Controller/OffersController.java
@@ -15,14 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import web.backend.gothere.Repositories.Entities.BarEntity;
-import web.backend.gothere.Repositories.Interfaces.BarRepository;
 import web.backend.gothere.Services.BarService;
 import web.backend.gothere.Services.OffersService;
 import web.backend.gothere.Services.Models.BarDTO;
 import web.backend.gothere.Services.Models.OfferDTO;
-
-
 
 @RestController
 @RequestMapping("api/offers")
@@ -30,38 +26,35 @@ public class OffersController {
     private final OffersService offersService;
     private final BarService barService;
 
-    OffersController (OffersService offersService, BarService barService) {
+    OffersController(OffersService offersService, BarService barService) {
         this.offersService = offersService;
         this.barService = barService;
     }
 
-    
     @GetMapping()
-    public List<OfferDTO> GetAllOffers(@RequestParam(required=false, name = "latitude") Double latitude, 
-    @RequestParam(required=false,name = "length") Double length,
-    @RequestParam(required=false,name = "distance") Double distance
-    ) throws ResponseStatusException {
-        
-        if(!offersService.getAll(latitude, length, distance).isEmpty()){
+    public List<OfferDTO> getAllOffers(@RequestParam(required = false, name = "latitude") Double latitude,
+            @RequestParam(required = false, name = "length") Double length,
+            @RequestParam(required = false, name = "distance") Double distance) throws ResponseStatusException {
+
+        if (!offersService.getAll(latitude, length, distance).isEmpty()) {
             return offersService.getAll(latitude, length, distance);
-        }else{
+        } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
     }
-   
+
     @GetMapping("/{id}")
-    public OfferDTO GetOffersById(@PathVariable("id") Long id) throws ResponseStatusException {
+    public OfferDTO getOffersById(@PathVariable("id") Long id) throws ResponseStatusException {
         return offersService.findbyOfferId(id);
     }
 
     @GetMapping("/bar/{id}")
-    public List<OfferDTO> GetBarOffersById(@PathVariable("id") Long id) throws ResponseStatusException {
+    public List<OfferDTO> getBarOffersById(@PathVariable("id") Long id) throws ResponseStatusException {
         return offersService.findOffersByBarId(id);
     }
 
-
     @PostMapping()
-    public  ResponseEntity<HttpStatus> addOffer(@RequestBody OfferDTO offer) {
+    public ResponseEntity<HttpStatus> addOffer(@RequestBody OfferDTO offer) {
         try {
             BarDTO bar = barService.getBarById(offer.getBar().getIdbar());
             OfferDTO offerToAdd = offer;
@@ -72,19 +65,19 @@ public class OffersController {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
     }
-    
-   @PutMapping("/{id}")
-   public ResponseEntity<HttpStatus> putMethodName(@PathVariable Long id, @RequestBody OfferDTO offer) {
-    try {
-        offersService.update(id, offer);
-        return new ResponseEntity<>(HttpStatus.OK);
-    } catch (Exception e) {
-        return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+
+    @PutMapping("/{id}")
+    public ResponseEntity<HttpStatus> updateOffer(@PathVariable Long id, @RequestBody OfferDTO offer) {
+        try {
+            offersService.update(id, offer);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+        }
     }
-   }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<HttpStatus> delete(@PathVariable("id") Long id) {
+    public ResponseEntity<HttpStatus> deleteOffer(@PathVariable("id") Long id) {
         try {
             offersService.deleteById(id);
             return new ResponseEntity<>(HttpStatus.OK);
@@ -92,7 +85,5 @@ public class OffersController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-   
-
 
 }
