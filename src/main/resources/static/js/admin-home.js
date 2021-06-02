7dd6e4686d57eb82401af065ce22c45061fb311c
Jorge Muñoz <jorge98mv@gmail.com>
2021-06-02 21:22:04 +0200

    Divide js

---
+++ src/main/resources/static/js/admin-home.js
@@ -0,0 +1,73 @@
+window.onload = () => {
+    if (/admin\/home/.test(window.location.href)) {
+        let button = document.querySelector("button.changes-button")
+        button.addEventListener('click', function(event) {
+            let idBar = parseInt(document.querySelector('#idbar').value)
+            let bar = {
+                idBar: parseInt(document.querySelector('#idbar').value),
+                name: document.querySelector('#name').innerHTML,
+                description: document.querySelector('#description').value,
+                address: document.querySelector('#address').value,
+                phone: parseInt(document.querySelector('#phone').value),
+                schedule: document.querySelector('#schedule').value,
+                currentCapacity: parseInt(document.querySelector('#currentCapacity').value),
+                totalCapacity: parseInt(document.querySelector('#totalCapacity').value),
+                allowedCapacity: parseInt(document.querySelector('#allowedCapacity').value),
+                length: parseInt(document.querySelector('#length').value),
+                latitude: parseInt(document.querySelector('#latitude').value),
+            }
+            fetch('/api/bars/' + idBar, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(bar),
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                    window.location = "/admin/home"
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        })
+        document.querySelector("#upload").addEventListener("click", (e) => {
+            var maxSize = 1048575;
+            if (!document.querySelector("#image").files[0])
+                return
+
+            if (document.querySelector("#image").files[0].size > maxSize) {
+                e.preventDefault();
+                alert(`Archivo demasiado grande (el archivo no puede ser mayor a ${Math.floor( maxSize/1e6)} Mb)`)
+            }
+
+        })
+        let deleteImgButtons = document.querySelectorAll('[id^="delete-img-"]')
+        deleteImgButtons.forEach(item => {
+            item.addEventListener('click', function(event) {
+
+                var idSplited = event.target.id.split("-")
+                let idImgBar = idSplited[2]
+                if (!confirm("Estás seguro de que quieres eliminar esta imagen (Esta acción es irreversible)")) {
+                    return
+                }
+
+                fetch('/admin/image/delete/' + idImgBar, {
+                        method: 'DELETE'
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log('Success:', data);
+                        window.location = "/admin/home"
+
+                    })
+                    .catch((error) => {
+                        alert("no se ha podido eliminar la imagen")
+                        console.error('Error:', error);
+                    });
+            })
+        })
+
+    }
+}
